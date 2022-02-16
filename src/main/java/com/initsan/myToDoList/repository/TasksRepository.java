5fec28b0ae67d85b53f2540c3fc3102db76d2930
initsan <initsan@yandex.ru>
2022-02-16 21:26:59 +0300

    добавлена возможность обработки только своих тасок

--- src/main/java/com/initsan/myToDoList/repository/TasksRepository.java
+++ src/main/java/com/initsan/myToDoList/repository/TasksRepository.java
@@ -3,8 +3,13 @@ package com.initsan.myToDoList.repository;
 import com.initsan.myToDoList.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TasksRepository extends JpaRepository<Task, Long> {
-    Optional<Task> findByTitle(String title);
+
+    Optional<Task> findByTitleAndUserId(String title, Long userId);
+    Optional<List<Task>> findTasksByUserId(Long userId);
+    Optional<Task> findByIdAndUserId(Long id, Long userId);
+
 }
